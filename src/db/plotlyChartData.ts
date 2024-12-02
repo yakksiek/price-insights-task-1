ba52a2f211cb7f2838ce302809c3dda973b58036
MarcinK <marcin.kulbicki@gmail.com>
2024-12-02 12:02:08 +0100

    config(plotlyChartData): change initial data

--- src/db/plotlyChartData.ts
+++ src/db/plotlyChartData.ts
@@ -1,18 +1,17 @@
-import { getCssVariable } from '../utils';
-import { pricingCampaignsData, pricingMonitoringData } from './pricingMonitoringData';
 import * as t from '../types';
+import { getCssVariable } from '../utils';
 
 const primaryBlue = getCssVariable('--primary-blue') || 'rgb(23, 106, 229)';
 const primaryPurple = getCssVariable('--primary-purple') || 'rgb(113, 7, 226)';
 
 export const plotlyChartConfigPricingCampaigns: t.PlotlyChartConfig = {
-    configPrimaryData: pricingCampaignsData.data,
+    configPrimaryData: 63,
     colorPrimary: primaryBlue,
     colorScheme: 'blue',
 };
 
 export const plotlyChartConfigMonitoringData: t.PlotlyChartConfig = {
-    configPrimaryData: pricingMonitoringData.data,
+    configPrimaryData: 67,
     colorPrimary: primaryPurple,
     colorScheme: 'purple',
 };
